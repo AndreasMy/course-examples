40855919d996cd01081f1e737dda13a2c860815f
AndreasMy <amykle85@gmail.com>
2023-02-23 21:03:01 +0100

    added new image

--- script/script.js
+++ script/script.js
@@ -1,7 +1,22 @@
 function slide() {
   let slideValue = document.querySelector("#slider").value;
-  document.querySelector("#overlayImg").style.clipPath = `polygon(0 0, ${slideValue}% 0, ${slideValue}% 100%, 0 100%)`;
+  document.querySelector(
+    "#overlayImg"
+  ).style.clipPath = `polygon(0 0, ${slideValue}% 0, ${slideValue}% 100%, 0 100%)`;
+
   console.log(
     "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)"
   );
 }
+
+function slide1() {
+    let slideValue = document.querySelector("#slider2").value;
+    document.querySelector(
+      "#overlayImg2"
+    ).style.clipPath = `polygon(0 0, ${slideValue}% 0, ${slideValue}% 100%, 0 100%)`;
+  
+    console.log(
+      "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)"
+    );
+  }
+  
