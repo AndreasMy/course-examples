dcc31dc933297f3967b23c5c31e3a45607fa4cf6
AndreasMy <amykle85@gmail.com>
2023-03-01 19:29:38 +0100

    navigation script should hopefully work in all scenarios now

--- script/script.js
+++ script/script.js
@@ -23,20 +23,31 @@ function slide1() {
 //? js knows the page location
 //? assign correct address location for nav buttons
 const findNum = /\d+/g;
+const findPath = /.*\/(.*)[0-9]*\.html/;
+
+//* Get path
 const path = window.location.pathname;
+const currentPath = path.match(findPath);
+
+//* Get html file name
+let pathName;
+if (currentPath && currentPath[1]) {
+  pathName = currentPath[1];
+}
+
+//* Extract number and convert to int
 const currentAddress = path.substring(6);
-const matchedNum = currentAddress.match(findNum);
+const matchedNum = pathName.match(findNum);
 let iForward = parseInt(matchedNum);
 let ibackward = parseInt(matchedNum);
-console.log(currentAddress)
-
 const decrementNum = --ibackward;
 const incrementNum = ++iForward;
 
-const address = currentAddress.substring(1, 8);
-let nextPage = `../pages/${address}${incrementNum}.html`;
-let previousPage = `../pages/${address}${decrementNum}.html`;
-console.log(previousPage);
+//* Remove number from pathName and concatenate string
+const addressName = pathName.replace(findNum, "");
+
+let nextPage = `../pages/${addressName}${incrementNum}.html`;
+let previousPage = `../pages/${addressName}${decrementNum}.html`;
 
 function navPrevious() {
   window.location.href = `${previousPage}`;
@@ -48,11 +59,11 @@ function navNext() {
 }
 
 function navHome() {
-  window.location.href = "../index.html"
+  window.location.href = "../index.html";
 }
 
+console.log(`addressName: ${addressName}`);
+console.log(`pathName: ${pathName}`);
 console.log(matchedNum, typeof matchedNum);
 console.log(decrementNum, incrementNum);
-console.log(address);
-console.log(currentAddress, address);
 console.log(previousPage, nextPage);
