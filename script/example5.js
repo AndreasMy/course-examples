6b1e219b223c6bcb39ab83567e641f4dafb96019
AndreasMy <amykle85@gmail.com>
2023-03-02 21:58:10 +0100

    added buttons to flip between image layers

--- script/example5.js
+++ script/example5.js
@@ -1,29 +1,102 @@
 const imgContainer = document.querySelector("#imgContainer");
+const valueScaleContainer = document.querySelector("#valueScaleContainer");
 
-function createColumn(amount) {
-  const valueScaleContainer = document.querySelector("#valueScaleContainer");
-/* 
-  valueScaleContainer.style = `grid-template-columns: repeat(1, 1fr);
-                                 grid-template-rows: repeat(15, 1fr);`; */
+//? Images
+const imgDefault = document.querySelector("#imgDefault");
+const imgDarker = document.querySelector("#imgDarker");
+const imgMiddle = document.querySelector("#imgMiddle");
+const imgBright = document.querySelector("#imgBright");
+const imgContrast = document.querySelector("#imgContrast");
+const imgColor = document.querySelector("#imgColor");
 
-  /*   for (let i = 0; i < 15; i++) {
-    let gridItem = document.createElement("div");
-    gridItem.className = "grid-item";
-    gridItem.setAttribute("id", "gridItem");
-    valueScaleContainer.appendChild(gridItem);
-  } */
-  //* create greyscale
+//? Buttons
+const btnColor = document.querySelector("#btnColor");
+const btnDefault = document.querySelector("#btnDefault");
+const btnDark =  document.querySelector("#btnDark");
+const btnHalftone = document.querySelector("#btnHalftone");
+const btnLigth = document.querySelector("#btnLigth");
+const btnConstrast = document.querySelector("#btnConstrast");
 
+//* create greyscale
+function createColumn() {
+  
   for (let i = 0; i < 10; i++) {
+
     let newRgb = 255 * 0.66 ** i;
     let gridItem = document.createElement("div");
     gridItem.className = `grid-item item${[i]}`;
+
       if (newRgb < 9) {
         newRgb = 0;
       }
+
     gridItem.style.backgroundColor = `rgb(${newRgb}, ${newRgb}, ${newRgb})`;
     valueScaleContainer.appendChild(gridItem);
   }
 }
 
 createColumn();
+
+//? loop through all divs
+
+btnColor.addEventListener("click", () => {
+  imgColor.style = `opacity: 100%;`
+
+  imgContrast.style = `opacity: 0%;`
+  imgDefault.style = `opacity: 0%;`
+  imgDarker.style = `opacity: 0%;`
+  imgMiddle.style = `opacity: 0%;`
+  imgBright.style = `opacity: 0%;`
+})
+
+btnDefault.addEventListener("click", () => {
+  imgDefault.style = `opacity: 100%;`
+
+  imgColor.style = `opacity: 0%;`
+  imgDarker.style = `opacity: 0%;`
+  imgMiddle.style = `opacity: 0%;`
+  imgBright.style = `opacity: 0%;`
+  imgContrast.style = `opacity: 0%;`
+
+
+})
+
+btnDark.addEventListener("click", () => {
+  imgDarker.style = `opacity: 100%;`
+
+  imgColor.style = `opacity: 0%;`
+  imgDefault.style = `opacity: 0%;`
+  imgMiddle.style = `opacity: 0%;`
+  imgBright.style = `opacity: 0%;`
+  imgContrast.style = `opacity: 0%;`
+})
+
+btnHalftone.addEventListener("click", () => {
+  imgMiddle.style = `opacity: 100%;`
+
+  imgColor.style = `opacity: 0%;`
+  imgDefault.style = `opacity: 0%;`
+  imgDarker.style = `opacity: 0%;`
+  imgBright.style = `opacity: 0%;`
+  imgContrast.style = `opacity: 0%;`
+})
+
+btnLigth.addEventListener("click", () => {
+  imgBright.style = `opacity: 100%;`
+
+  imgColor.style = `opacity: 0%;`
+  imgDefault.style = `opacity: 0%;`
+  imgDarker.style = `opacity: 0%;`
+  imgMiddle.style = `opacity: 0%;`
+  imgContrast.style = `opacity: 0%;`
+})
+
+btnConstrast.addEventListener("click", () => {
+  imgContrast.style = `opacity: 100%;`
+
+  imgColor.style = `opacity: 0%;`
+  imgDefault.style = `opacity: 0%;`
+  imgDarker.style = `opacity: 0%;`
+  imgMiddle.style = `opacity: 0%;`
+  imgBright.style = `opacity: 0%;`
+})
